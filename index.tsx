027f7eb2c91181095b5239ca75b303407d85bd36
kot :3 <65986892+KotTheKitty@users.noreply.github.com>
2026-01-13 00:43:07 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -267,7 +267,7 @@ export default definePlugin({
 
       const mbRes = await fetch(
         `https://musicbrainz.org/ws/2/release/?query=
-          release:${encodeURIComponent(albumName)}&
+          release:${encodeURIComponent(albumName)}%20AND%20
           artist:${encodeURIComponent(artistNameMB)}&fmt=json`,
       );
 
